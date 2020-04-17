4a2a93685abd148c9cd1961de7d9cb714fa1e990
Daria <purumvisum@gmail.com>
2020-04-17 11:55:36 +0200

    fix vulnerabilities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,109 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { request } from "graphql-request";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const url = `http://localhost:4000/`;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const ArticlesQuery = `
+      query ($published: Boolean){ 
+        articles(search: {published: $published}) { 
+          id
+          title 
+          isPublished
+        } 
+      }
+    `;
+
+const UsersQuery = `
+  query($limit: Int) {
+    users(limit: $limit) {
+      id
+      name
+    }
+  }
+`;
+
+const PublishArticleMutation = `
+  mutation PublishArticle($articleId: ID!) {
+    publishArticle(id: $articleId) {
+      id
+    }
+  }
+`;
+
+function App() {
+    const [articles, setArticles] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [limit, setLimit] = useState(3);
+
+    const fetchArticles = async () => {
+        const { articles } = await request(url, ArticlesQuery);
+        setArticles(articles);
+
+        // articles.forEach((art) => {
+        //     console.log('article.isPublished', art.isPublished)
+        // })
+    };
+
+    const fetchUsers = async () => {
+        const { users } = await request(url, UsersQuery, { limit });
+        setUsers(users);
+    };
+
+    const publishArticle = async articleId => {
+        await request(url, PublishArticleMutation, { articleId });
+        fetchArticles();
+    };
+
+    useEffect(() => {
+        fetchArticles();
+        fetchUsers();
+    }, []);
+
+
+
+
+    return (
+        <div className="App">
+            <h1>Large</h1>
+            <h3>The best alternative to Medium</h3>
+
+            <br />
+            <div>
+                <input
+                    onChange={e => setLimit(parseInt(e.target.value))}
+                    value={limit}
+                    placeholder="limit"
+                    type="number"
+                />
+                <button onClick={fetchUsers}> fetch users </button>
+            </div>
+
+            <h3> Users </h3>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+
+            <div className="flex-h">
+                <h3> Articles </h3>
+                <button onClick={fetchArticles}> fetch articles </button>
+            </div>
+
+            {articles.map(article => (
+                <div className="article" key={article.id}>
+                    <div style={{ opacity: article.isPublished ? 1 : 0.3 }}>
+                        {article.title}
+                    </div>
+                    {!article.isPublished && (
+                        <button onClick={() => publishArticle(article.id)}>publish</button>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
